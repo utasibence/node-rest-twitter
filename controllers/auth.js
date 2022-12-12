ef8226cfcce6da56cee7978dbe2b5841b171e314
Bence Utasi <butasi@diligent.com>
2022-12-12 12:54:43 +0100

    WIP

---
+++ controllers/auth.js
@@ -0,0 +1,74 @@
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Valodation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hash,
+      name: name,
+    });
+
+    const result = await user.save();
+
+    res.status(201).json({ message: 'User created!', userId: result._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error('A user with this email could not be found.');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('Wrong password!');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString(),
+      },
+      'secret',
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ token: token, userId: user._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
---
+++ routes/feed.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.get('/posts', isAuth, feedController.getPosts);
+
+router.get('/status', isAuth, feedController.getStatus);
+
+router.put(
+  '/status',
+  isAuth,
+  body('status').trim().not().isEmpty(),
+  feedController.updateStatus
+);
+
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
+
+module.exports = router;
